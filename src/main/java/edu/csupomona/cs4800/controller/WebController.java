66c98f980ede8ab45f0abd07b4f0d042a83c3f97
jmsato <jordyn.sato@gmail.com>
2020-09-22 01:27:35 -0700

    Cleaned up controllers

--- src/main/java/edu/csupomona/cs4800/controller/WebController.java
+++ src/main/java/edu/csupomona/cs4800/controller/WebController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class WebController {
 
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String endpoints() {
+		return String.format("There are other pages at: /hello and /serena");
+	}
+	
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
