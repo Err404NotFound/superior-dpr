db7d9898349db8e4476d668fb3599727fe928ef9
Serena Ing <sming@cpp.edu>
2020-09-23 16:19:35 -0700

    Adding pdfbox capabilities

--- src/main/java/edu/csupomona/cs4800/controller/HelloController.java
+++ src/main/java/edu/csupomona/cs4800/controller/HelloController.java
@@ -1,9 +1,16 @@
 package edu.csupomona.cs4800.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;   
+import java.io.File;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 @RestController
 public class HelloController {
@@ -17,4 +24,61 @@ public class HelloController {
 	public String serena(@RequestParam(value = "something", defaultValue = "serena") String name) {
 		return String.format("%s is very tired!", name);
 	}
+	
+	@RequestMapping(value = "/pdfbox", method = RequestMethod.GET)
+
+		public String pdfbox(@RequestParam(value = "something", defaultValue = "pdfbox") String name)  throws IOException
+	    {
+	        //String fileName = "C:\\Users\\singi\\Desktop\\cs4800a3\\ihatemylife\\demo-web-project\\visitor\\src\\main\\java\\edu\\csupomona\\cs356\\sample.pdf"; // provide the path to pdf file
+	        String fileName=".\\sample.pdf";
+	    	
+	    	PDDocument document = null;
+	        String text="";
+	        try
+	        {
+	            document = PDDocument.load( new File(fileName));
+	            PDFTextStripper stripper = new PDFTextStripper();
+	            String pdfText = stripper.getText(document).toString();
+	            System.out.println( "Text in the area:" + pdfText);
+	            text=pdfText;
+	        }
+	        finally
+	        {
+	            if( document != null )
+	            {
+	                document.close();
+	            }
+	        }
+	        return String.format("Text in the area:%s", text);
+	    }
+
+	//		//Creating PDF document object   
+//		String fileName="sample.pdf";
+//		PDDocument document = null;
+//        try {
+//        	document=PDDocument.load(new File(fileName));
+//        	PDFTextStripper stripper = new PDFTextStripper();
+//        	String pdfText = stripper.getText(document).toString();
+//        	System.out.println("Text in the area:"+pdfText);
+//			//PDDocument doc = new PDDocument();      
+//	    
+//	        ////Saving the document  
+//	        //doc.save("/eclipse-workspace/blank.pdf");  
+//	        
+//	        //System.out.println("PDF created");    
+//	    
+//	        //Closing the document    
+//	        // doc.close(); 
+//	        //return String.format("Created a pdf");
+//        }
+//        finally {
+//        	if(document !=null) {
+//        		document.close();
+//        	}
+//        }
+//        return String.format("Created a pdf");
+//        //catch(Exception e) {
+//        //	return String.format("Error occurred, please try again");
+//        //}		
+//	}
 }
