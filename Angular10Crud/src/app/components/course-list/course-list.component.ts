888c3e54761ca1c84e30dcfbfb2f19d5537e965c
SerenaIng <sming@cpp.edu>
2020-11-24 00:56:54 -0800

    Using ng bootstrap componenet

--- Angular10Crud/src/app/components/course-list/course-list.component.ts
+++ Angular10Crud/src/app/components/course-list/course-list.component.ts
@@ -1,5 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
 import { CourseService } from 'app/services/course.service';
+import { merge, Observable, Subject } from 'rxjs';
+import {debounceTime, distinctUntilChanged, filter, map} from 'rxjs/operators';
+
+const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
+  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
+  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
+  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
 
 @Component({
@@ -8,13 +20,34 @@ import { CourseService } from 'app/services/course.service';
   styleUrls: ['./course-list.component.css']
 })
 export class CourseListComponent implements OnInit {
-
-  courses: any;
+  model: any;
+  courses: Array<any>;
+  coursesNotJson:Array<any>=[];
   currentCourse = null;
   currentIndex=1;
   courseName='';
 
   constructor(private courseService: CourseService) { }
+
+  
+
+  @ViewChild('instance', {static: true}) instance: NgbTypeahead;
+  focus$ = new Subject<string>();
+  click$ = new Subject<string>();
+
+  search = (text$: Observable<string>) => {
+    const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+    const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
+    const inputFocus$ = this.focus$;
+    
+    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+      map(term => (term === '' ? this.coursesNotJson
+        : this.coursesNotJson.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10)),
+
+    );
+
+  }
+
 
   ngOnInit(): void {
     this.retrieveCourses();
@@ -25,6 +58,9 @@ export class CourseListComponent implements OnInit {
       .subscribe(
         data=>{
           this.courses=data;
+          this.courses.forEach(course=>{
+            this.coursesNotJson.push(course.courseNumber +" "+ course.courseName);
+          })
           console.log(data);
         },
         error =>{
@@ -41,6 +77,10 @@ export class CourseListComponent implements OnInit {
   setActiveCourse(course, index): void{
     this.currentCourse=course;
     this.currentIndex=index;
+
+    // this.currentIndex=this.coursesNotJson.indexOf(this.model);
+    // this.currentCourse=this.courses[this.currentIndex]; 
+    // console.log('shit: ' +this.currentCourse + " " + this.currentIndex);
   }
 /*
   removeAllTutorials(): void {
@@ -56,14 +96,18 @@ export class CourseListComponent implements OnInit {
   }
 */
   searchTitle(): void {
-    this.courseService.findByCourseName(this.courseName)
-      .subscribe(
-        data => {
-          this.courses = data;
-          console.log(data);
-        },
-        error => {
-          console.log(error);
-        });
+    this.currentIndex=this.coursesNotJson.indexOf(this.model);
+    this.currentCourse=this.courses[this.currentIndex]; 
+    console.log('shit: ' +this.currentCourse + " " + this.currentIndex);
+    
+    // this.courseService.findByCourseName(this.courseName)
+    //   .subscribe(
+    //     data => {
+    //       this.courses = data;
+    //       console.log(data);
+    //     },
+    //     error => {
+    //       console.log(error);
+    //     });
   }
 }
